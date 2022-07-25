90fa105ae1f529597502748833fa30fef1ad7012
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2022-07-25 11:57:22 +0200

    minor cleanings..

---
+++ tests/test_solver.cpp
@@ -0,0 +1,29 @@
+#include "solver.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        std::cerr << "usage: oRatio <input-file> [<input-file> ...] <output-file>\n";
+        return -1;
+    }
+
+    // the problem files..
+    std::vector<std::string> prob_names;
+    for (int i = 1; i < argc - 1; i++)
+        prob_names.push_back(argv[i]);
+
+    // the solution file..
+    std::string sol_name = argv[argc - 1];
+
+    std::cout << "starting oRatio";
+#ifndef NDEBUG
+    std::cout << " in debug mode";
+#endif
+    std::cout << "..\n";
+
+    for (size_t i = 0; i < NUM_TESTS; ++i)
+    {
+    }
+}
