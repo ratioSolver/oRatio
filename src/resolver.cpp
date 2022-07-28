067b5fda41d58ff83d57cbc96559119c12e849f3
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2022-07-28 11:51:38 +0200

    solved minor issue..

--- src/resolver.cpp
+++ src/resolver.cpp
@@ -5,7 +5,7 @@
 
 namespace ratio::solver
 {
-    resolver::resolver(semitone::rational cost, flaw &eff) : resolver(semitone::lit(eff.get_solver().get_sat()->new_var()), std::move(cost), eff) {}
+    resolver::resolver(semitone::rational cost, flaw &eff) : resolver(semitone::lit(eff.get_solver().get_sat_core()->new_var()), std::move(cost), eff) {}
     resolver::resolver(semitone::lit r, semitone::rational cost, flaw &eff) : slv(eff.get_solver()), rho(std::move(r)), intrinsic_cost(std::move(cost)), effect(eff) {}
 
     ORATIO_EXPORT semitone::rational resolver::get_estimated_cost() const noexcept { return slv.get_causal_graph().get_estimated_cost(*this); }
