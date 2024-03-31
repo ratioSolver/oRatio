3db3a3882c040884bd98be5d496a64db3cc048da
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-03-31 19:23:00 +0200

    Fix assert in bool_flaw constructor

--- src/flaws/bool_flaw.cpp
+++ src/flaws/bool_flaw.cpp
@@ -5,6 +5,6 @@ namespace ratio
 {
     bool_flaw::bool_flaw(solver &s, std::vector<std::reference_wrapper<resolver>> &&causes, std::shared_ptr<riddle::bool_item> b_item) noexcept : flaw(s, std::move(causes)), b_item(b_item)
     {
-        assert(b_item);
+        assert(b_item.get());
     }
 } // namespace ratio
