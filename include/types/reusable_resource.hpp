efa0583323ed72119c0ccdc17ff7ae351eaccf3a
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-04-18 13:02:39 +0200

    Add reusable_resource flaw and resolver classes

--- include/types/reusable_resource.hpp
+++ include/types/reusable_resource.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "smart_type.hpp"
+#include "flaw.hpp"
+#include "resolver.hpp"
 
 namespace ratio
 {
@@ -13,13 +15,89 @@ namespace ratio
     reusable_resource(solver &slv);
 
   private:
-    std::vector<std::vector<std::pair<utils::lit, double>>> get_current_incs() noexcept override { return {}; }
+    std::vector<std::vector<std::pair<utils::lit, double>>> get_current_incs() noexcept override;
 
     void new_atom(std::shared_ptr<ratio::atom> &atm) noexcept override;
 
 #ifdef ENABLE_VISUALIZATION
     json::json extract() const noexcept override;
 #endif
+
+    /**
+     * @brief Represents a reusable resource flaw.
+     *
+     * A reusable resource flaw is a set two or more atoms that temporally overlap in the same reusable resource instance.
+     *
+     * This class inherits from the `flaw` class and represents a flaw related to a reusable resource.
+     * It provides functionality to compute resolvers for the flaw.
+     */
+    class rr_flaw final : public flaw
+    {
+    public:
+      rr_flaw(reusable_resource &rr, const std::set<atom *> &mcs);
+
+    private:
+      void compute_resolvers() override;
+
+      /**
+       * @brief Represents a resolver that orders two atoms.
+       *
+       * The `order_resolver` class is a concrete implementation of the `resolver` base class.
+       * It is used to specify the order between two atoms, where applying the resolver will order
+       * the `before` atom before the `after` atom.
+       */
+      class order_resolver final : public resolver
+      {
+      public:
+        /**
+         * @brief Constructs an `order_resolver` object.
+         *
+         * @param flw The reference to the `rr_flaw` object.
+         * @param r The reference to the `lit` object.
+         * @param before The reference to the `atom` object that should be ordered before the other.
+         * @param after The reference to the `atom` object that should be ordered after the other.
+         */
+        order_resolver(rr_flaw &flw, const utils::lit &r, const atom &before, const atom &after);
+
+      private:
+        void apply() override {}
+
+      private:
+        const atom &before; // applying the resolver will order this atom before the other..
+        const atom &after;  // applying the resolver will order this atom after the other..
+      };
+
+      /**
+       * @brief A resolver that forbids a specific atom on a given reusable resource.
+       *
+       * This resolver is used to forbid a specific atom on a given reusable resource in the solver.
+       * It is a subclass of the base class `resolver`.
+       */
+      class forbid_resolver final : public resolver
+      {
+      public:
+        /**
+         * @brief Constructs a forbid_resolver object.
+         *
+         * @param flw The reference to the rr_flaw object.
+         * @param r The reference to the utils::lit object.
+         * @param atm The reference to the atom object.
+         * @param itm The reference to the riddle::component object.
+         */
+        forbid_resolver(rr_flaw &flw, const utils::lit &r, atom &atm, riddle::component &itm);
+
+      private:
+        void apply() override {}
+
+      private:
+        atom &atm;              // applying the resolver will forbid this atom on the `itm` reusable resource..
+        riddle::component &itm; // applying the resolver will forbid the `atm` atom on this reusable resource..
+      };
+
+    private:
+      reusable_resource &rr;
+      const std::set<atom *> mcs;
+    };
 
     class rr_atom_listener final : private atom_listener
     {
@@ -39,6 +117,7 @@ namespace ratio
   private:
     std::set<const riddle::item *> to_check;                        // the reusable-resource instances whose atoms have changed..
     std::vector<std::reference_wrapper<atom>> atoms;                // the atoms of the reusable-resource..
+    std::set<std::set<atom *>> rr_flaws;                            // the reusable-resource flaws found so far..
     std::vector<std::unique_ptr<rr_atom_listener>> listeners;       // we store, for each atom, its atom listener..
     std::map<atom *, std::map<atom *, utils::lit>> leqs;            // all the possible ordering constraints..
     std::map<atom *, std::map<utils::enum_val *, utils::lit>> frbs; // all the possible forbidding constraints..
--- src/types/reusable_resource.cpp
+++ src/types/reusable_resource.cpp
@@ -1,10 +1,114 @@
 #include <cassert>
 #include "reusable_resource.hpp"
-#include "solver.hpp"
+#include "graph.hpp"
+#include "combinations.hpp"
 
 namespace ratio
 {
     reusable_resource::reusable_resource(solver &slv) : smart_type(slv, "ReusableResource") {}
+
+    std::vector<std::vector<std::pair<utils::lit, double>>> reusable_resource::get_current_incs() noexcept
+    {
+        std::vector<std::vector<std::pair<utils::lit, double>>> incs; // the inconsistencies..
+        // we assign the atoms to the reusable-resources that need to be checked..
+        std::unordered_map<riddle::component *, std::vector<atom *>> rr_instances;
+        for (const auto &atm : atoms)
+            if (get_solver().get_sat().value(atm.get().get_sigma()) == utils::True)
+            { // the atom is active..
+                const auto tau = atm.get().get("tau");
+                if (is_enum(*tau))
+                { // the `tau` parameter is a variable..
+                    for (const auto &c_rr : get_solver().domain(static_cast<const riddle::enum_item &>(*tau)))
+                        if (to_check.find(static_cast<const riddle::item *>(&c_rr.get())) != to_check.end())
+                            rr_instances[static_cast<riddle::component *>(&c_rr.get())].push_back(&atm.get());
+                }
+                else if (to_check.find(tau.get()) != to_check.end()) // the `tau` parameter is a constant..
+                    rr_instances[static_cast<riddle::component *>(tau.get())].push_back(&atm.get());
+            }
+
+        // we detect inconsistencies for each of the reusable-resource instances..
+        for ([[maybe_unused]] const auto &[rr, atms] : rr_instances)
+        {
+            // for each pulse, the atoms starting at that pulse..
+            std::map<utils::inf_rational, std::set<atom *>> starting_atoms;
+            // for each pulse, the atoms ending at that pulse..
+            std::map<utils::inf_rational, std::set<atom *>> ending_atoms;
+            // all the pulses of the timeline, sorted..
+            std::set<utils::inf_rational> pulses;
+            // the resource capacity..
+            auto c_capacity = get_solver().arithmetic_value(static_cast<riddle::arith_item &>(*rr->get("capacity")));
+
+            for (const auto &atm : atms)
+            {
+                const auto start = get_solver().arithmetic_value(*std::static_pointer_cast<riddle::arith_item>(atm->get("start")));
+                const auto end = get_solver().arithmetic_value(*std::static_pointer_cast<riddle::arith_item>(atm->get("end")));
+                starting_atoms[start].insert(atm);
+                ending_atoms[end].insert(atm);
+                pulses.insert(start);
+                pulses.insert(end);
+            }
+
+            // we scroll through the timeline looking for inconsistencies..
+            bool has_conflict = false;
+            std::set<atom *> overlapping_atoms;
+            for (const auto &p : pulses)
+            {
+                // we check if there are atoms that start at `p`, and if there are, we add them to the set of overlapping atoms..
+                if (const auto at_start_p = starting_atoms.find(p); at_start_p != starting_atoms.cend())
+                    overlapping_atoms.insert(at_start_p->second.cbegin(), at_start_p->second.cend());
+                // we check if there are atoms that end at `p`, and if there are, we remove them from the set of overlapping atoms..
+                if (const auto at_end_p = ending_atoms.find(p); at_end_p != ending_atoms.cend())
+                    for (const auto &a : at_end_p->second)
+                        overlapping_atoms.erase(a);
+
+                utils::inf_rational c_usage; // the concurrent resource usage..
+                for (const auto &a : overlapping_atoms)
+                    c_usage += get_solver().arithmetic_value(static_cast<riddle::arith_item &>(*a->get("usage")));
+
+                // if the resource usage exceeds the resource capacity, we have a conflict..
+                if (c_usage > c_capacity)
+                {
+                    has_conflict = true;
+                    // we extract the Minimal Conflict Sets (MCSs)..
+                    // we sort the overlapping atoms, according to their resource usage, in descending order..
+                    std::vector<atom *> inc_atoms(overlapping_atoms.cbegin(), overlapping_atoms.cend());
+                    std::sort(inc_atoms.begin(), inc_atoms.end(), [this](const auto &atm0, const auto &atm1)
+                              { return get_solver().arithmetic_value(static_cast<riddle::arith_item &>(*atm0->get("usage"))) > get_solver().arithmetic_value(static_cast<riddle::arith_item &>(*atm1->get("usage"))); });
+
+                    utils::inf_rational mcs_usage;       // the concurrent mcs resource usage..
+                    auto mcs_begin = inc_atoms.cbegin(); // the beginning of the current mcs..
+                    auto mcs_end = inc_atoms.cbegin();   // the end of the current mcs..
+                    while (mcs_end != inc_atoms.cend())
+                    {
+                        // we increase the size of the current mcs..
+                        while (mcs_usage <= c_capacity && mcs_end != inc_atoms.cend())
+                        {
+                            mcs_usage += get_solver().arithmetic_value(static_cast<riddle::arith_item &>(*(*mcs_end)->get("usage")));
+                            ++mcs_end;
+                        }
+
+                        if (mcs_usage > c_capacity)
+                        {
+                            std::set<atom *> mcs(mcs_begin, mcs_end); // the MCS..
+                            // we check if the MCS has already been found..
+                            if (rr_flaws.find(mcs) == rr_flaws.cend())
+                            {
+                                // we add the MCS to the set of found flaws..
+                                rr_flaws.insert(mcs);
+                                get_solver().get_graph().new_flaw<rr_flaw>(*this, mcs);
+                            }
+                        }
+
+                        // we decrease the size of the current mcs..
+                        mcs_usage -= get_solver().arithmetic_value(static_cast<riddle::arith_item &>(*(*mcs_begin)->get("usage")));
+                        assert(mcs_usage <= c_capacity);
+                        ++mcs_begin;
+                    }
+                }
+            }
+        }
+        return incs;
+    }
 
     void reusable_resource::new_atom(std::shared_ptr<ratio::atom> &atm) noexcept
     {
@@ -53,6 +157,13 @@ namespace ratio
         listener->something_changed();
         listeners.push_back(std::move(listener));
     }
+
+    reusable_resource::rr_flaw::rr_flaw(reusable_resource &rr, const std::set<atom *> &mcs) : flaw(rr.get_solver(), smart_type::get_resolvers(mcs)), rr(rr), mcs(mcs) {}
+    void reusable_resource::rr_flaw::compute_resolvers() {}
+
+    reusable_resource::rr_flaw::order_resolver::order_resolver(rr_flaw &flw, const utils::lit &r, const atom &before, const atom &after) : resolver(flw, r, utils::rational::zero), before(before), after(after) {}
+
+    reusable_resource::rr_flaw::forbid_resolver::forbid_resolver(rr_flaw &flw, const utils::lit &r, atom &atm, riddle::component &itm) : resolver(flw, r, utils::rational::zero), atm(atm), itm(itm) {}
 
     reusable_resource::rr_atom_listener::rr_atom_listener(reusable_resource &rr, atom &a) : atom_listener(a), rr(rr) {}
     void reusable_resource::rr_atom_listener::something_changed()
