24b462091ba87cf2358bda751ba281dd0008f64e
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-01-16 15:58:42 +0100

    Add navigation for Timelines and Graph components in the GUI

--- gui/app/src/main.ts
+++ gui/app/src/main.ts
@@ -1,11 +1,11 @@
-import { Settings, AppComponent, App, Connection, SolverComponent, solver } from 'ratio-lib';
+import { Settings, AppComponent, App, Connection, solver, TimelinesChart, SolverGraph } from 'ratio-lib';
 import './styles.css';
 
 Settings.get_instance().load_settings({ port: 8080, ws_path: 'ratio' });
 
 class oRatio extends AppComponent implements solver.SolverSetListener {
 
-  solver_component: SolverComponent | null = null;
+  private solver: solver.Solver | null = null;
 
   constructor() {
     super();
@@ -20,13 +20,48 @@ class oRatio extends AppComponent implements solver.SolverSetListener {
     brand.href = '#';
     brand.textContent = 'oRatio';
     container.appendChild(brand);
+
+    const pills = document.createElement('ul');
+    pills.classList.add('nav', 'nav-pills', 'ml-2');
+
+    const timelines_pill = document.createElement('li');
+    timelines_pill.classList.add('nav-item');
+    timelines_pill.role = 'presentation';
+    const timelines_pill_link = document.createElement('button');
+    timelines_pill_link.classList.add('nav-link', 'active');
+    timelines_pill_link.id = 'timelines-tab';
+    timelines_pill_link.setAttribute('data-bs-toggle', 'pill');
+    timelines_pill_link.setAttribute('role', 'tab');
+    timelines_pill_link.setAttribute('aria-controls', 'timelines');
+    timelines_pill_link.setAttribute('aria-selected', 'true');
+    timelines_pill_link.innerText = 'Timelines';
+    timelines_pill_link.addEventListener('click', () => { App.get_instance().selected_component(new TimelinesChart(this.solver!)); });
+    timelines_pill.appendChild(timelines_pill_link);
+    pills.appendChild(timelines_pill);
+
+    const graph_pill = document.createElement('li');
+    graph_pill.classList.add('nav-item');
+    graph_pill.role = 'presentation';
+    const graph_pill_link = document.createElement('button');
+    graph_pill_link.classList.add('nav-link');
+    graph_pill_link.id = 'graph-tab';
+    graph_pill_link.setAttribute('data-bs-toggle', 'pill');
+    graph_pill_link.setAttribute('role', 'tab');
+    graph_pill_link.setAttribute('aria-controls', 'graph');
+    graph_pill_link.setAttribute('aria-selected', 'false');
+    graph_pill_link.innerText = 'Graph';
+    graph_pill_link.addEventListener('click', () => { App.get_instance().selected_component(new SolverGraph(this.solver!)); });
+    graph_pill.appendChild(graph_pill_link);
+    pills.appendChild(graph_pill);
+
+    container.appendChild(pills);
   }
 
   init(solvers: Map<number, solver.Solver>): void {
     if (solvers.size !== 1)
       throw new Error('Expected exactly one solver');
-    this.solver_component = new SolverComponent(solvers.values()!.next()!.value!);
-    App.get_instance().selected_component(this.solver_component);
+    this.solver = solvers.values().next().value!;
+    App.get_instance().selected_component(new TimelinesChart(this.solver));
   }
   solver_created(_: solver.Solver): void { }
   solver_deleted(_: number): void { }
@@ -35,4 +70,3 @@ class oRatio extends AppComponent implements solver.SolverSetListener {
 }
 
 new oRatio();
-App.get_instance().selected_component(null);
