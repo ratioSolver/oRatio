936c52936c91493bb2a93794055e450f546d5e7e
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-10-07 17:53:52 +0200

    Enhance solver class with new integer, real, and time expression methods; improve boolean expression handling

--- include/solver.hpp
+++ include/solver.hpp
@@ -14,6 +14,21 @@ namespace ratio
     [[nodiscard]] riddle::bool_expr new_bool(const bool value) override;
     [[nodiscard]] utils::lbool bool_value(const riddle::bool_term &expr) const noexcept override;
 
+    [[nodiscard]] riddle::arith_expr new_int() override;
+    [[nodiscard]] riddle::arith_expr new_int(const INT_TYPE value) override;
+    [[nodiscard]] riddle::arith_expr new_int(const INT_TYPE lb, const INT_TYPE ub) override;
+    [[nodiscard]] riddle::arith_expr new_uncertain_int(const INT_TYPE lb, const INT_TYPE ub) override;
+
+    [[nodiscard]] riddle::arith_expr new_real() override;
+    [[nodiscard]] riddle::arith_expr new_real(utils::rational &&value) override;
+    [[nodiscard]] riddle::arith_expr new_real(utils::rational &&lb, utils::rational &&ub) override;
+    [[nodiscard]] riddle::arith_expr new_uncertain_real(utils::rational &&lb, utils::rational &&ub) override;
+
+    [[nodiscard]] riddle::arith_expr new_time() override;
+    [[nodiscard]] riddle::arith_expr new_time(utils::rational &&value) override;
+
+    [[nodiscard]] utils::inf_rational arith_value(const riddle::arith_term &expr) const noexcept override;
+
   private:
     std::vector<utils::lbool> assigns; // for each variable, the current assignment..
     linspire::solver lin_slv;          // the linear solver..
--- src/solver.cpp
+++ src/solver.cpp
@@ -1,17 +1,42 @@
 #include "solver.hpp"
+#include "items.hpp"
+#include <cassert>
 
 namespace ratio
 {
-    solver::solver(std::string_view name) noexcept : riddle::core(name), assigns() {}
-
-    riddle::bool_expr solver::new_bool() { return new_bool(utils::Undefined); }
-
-    riddle::bool_expr solver::new_bool(const bool value)
+    solver::solver(std::string_view name) noexcept : riddle::core(name)
     {
-        auto var = riddle::core::new_bool();
-        assigns.push_back(value ? utils::True : utils::False);
-        return var;
+        assigns[utils::FALSE_var] = utils::False; // the false constant..
     }
 
-    utils::lbool solver::bool_value(const riddle::bool_term &expr) const noexcept { return assigns.at(expr.get_id()); }
+    riddle::bool_expr solver::new_bool()
+    {
+        const auto x = assigns.size();
+        assigns.push_back(utils::Undefined);
+        return std::make_shared<riddle::bool_item>(static_cast<riddle::bool_type &>(get_type(riddle::bool_kw)), x);
+    }
+    riddle::bool_expr solver::new_bool(const bool value)
+    {
+        auto l = value ? utils::TRUE_lit : utils::FALSE_lit;
+        return std::make_shared<riddle::bool_item>(static_cast<riddle::bool_type &>(get_type(riddle::bool_kw)), std::move(l));
+    }
+    utils::lbool solver::bool_value(const riddle::bool_term &expr) const noexcept
+    {
+        return assigns.at(expr.get_id());
+    }
+
+    riddle::arith_expr solver::new_int() { return std::make_shared<riddle::arith_item>(static_cast<riddle::int_type &>(get_type(riddle::int_kw)), lin_slv.new_var()); }
+    riddle::arith_expr solver::new_int(const INT_TYPE value) { return std::make_shared<riddle::arith_item>(static_cast<riddle::int_type &>(get_type(riddle::int_kw)), utils::rational(value)); }
+    riddle::arith_expr solver::new_int(const INT_TYPE lb, const INT_TYPE ub) { return std::make_shared<riddle::arith_item>(static_cast<riddle::int_type &>(get_type(riddle::int_kw)), lin_slv.new_var(utils::rational(lb), utils::rational(ub))); }
+    riddle::arith_expr solver::new_uncertain_int(const INT_TYPE lb, const INT_TYPE ub) { return std::make_shared<riddle::arith_item>(static_cast<riddle::int_type &>(get_type(riddle::int_kw)), lin_slv.new_var(utils::rational(lb), utils::rational(ub))); }
+
+    riddle::arith_expr solver::new_real() { return std::make_shared<riddle::arith_item>(static_cast<riddle::real_type &>(get_type(riddle::real_kw)), lin_slv.new_var()); }
+    riddle::arith_expr solver::new_real(utils::rational &&value) { return std::make_shared<riddle::arith_item>(static_cast<riddle::real_type &>(get_type(riddle::real_kw)), std::move(value)); }
+    riddle::arith_expr solver::new_real(utils::rational &&lb, utils::rational &&ub) { return std::make_shared<riddle::arith_item>(static_cast<riddle::real_type &>(get_type(riddle::real_kw)), lin_slv.new_var(std::move(lb), std::move(ub))); }
+    riddle::arith_expr solver::new_uncertain_real(utils::rational &&lb, utils::rational &&ub) { return std::make_shared<riddle::arith_item>(static_cast<riddle::real_type &>(get_type(riddle::real_kw)), lin_slv.new_var(std::move(lb), std::move(ub))); }
+
+    riddle::arith_expr solver::new_time() { return std::make_shared<riddle::arith_item>(static_cast<riddle::time_type &>(get_type(riddle::time_kw)), lin_slv.new_var()); }
+    riddle::arith_expr solver::new_time(utils::rational &&value) { return std::make_shared<riddle::arith_item>(static_cast<riddle::time_type &>(get_type(riddle::time_kw)), std::move(value)); }
+
+    utils::inf_rational solver::arith_value(const riddle::arith_term &expr) const noexcept { return lin_slv.val(static_cast<const riddle::arith_item &>(expr).get_lin()); }
 } // namespace ratio
