ef7572006c061fe511612bcc47b37791cb9e89df
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-12-01 10:35:18 +0100

    Refactor basic solver and server components
    - Removed the implementation of basic_solver, basic_types, and basic_server.
    - Consolidated solver logic into solver_core.
    - Updated main entry point to remove server-related code.
    - Cleaned up unused includes and definitions.
    - Enhanced node and flaw management within the solver.
    - Improved JSON serialization for solver state and nodes.

--- include/solver_core.hpp
+++ include/solver_core.hpp
@@ -6,62 +6,11 @@
 
 namespace ratio
 {
-  struct context
+  class resolver : public riddle::resolver
   {
-    linspire::constraint lin_cnsts;                                            // The linear constraints in the current context..
-    std::vector<std::reference_wrapper<arc_consistency::constraint>> ac_cnsts; // The arc consistency constraints in the current context..
   };
 
-  class solver_core : public riddle::core
+  class solver : public riddle::core
   {
-  public:
-    solver_core(std::string_view name = "oRatio") noexcept;
-
-    [[nodiscard]] riddle::bool_expr new_bool() override;
-    [[nodiscard]] riddle::bool_expr new_bool(const bool value) override;
-    [[nodiscard]] utils::lbool bool_value(const riddle::bool_term &expr) const noexcept override;
-
-    [[nodiscard]] riddle::arith_expr new_int() override;
-    [[nodiscard]] riddle::arith_expr new_int(const INT_TYPE value) override;
-    [[nodiscard]] riddle::arith_expr new_int(const INT_TYPE lb, const INT_TYPE ub) override;
-    [[nodiscard]] riddle::arith_expr new_uncertain_int(const INT_TYPE lb, const INT_TYPE ub) override;
-
-    [[nodiscard]] riddle::arith_expr new_real() override;
-    [[nodiscard]] riddle::arith_expr new_real(utils::rational &&value) override;
-    [[nodiscard]] riddle::arith_expr new_real(utils::rational &&lb, utils::rational &&ub) override;
-    [[nodiscard]] riddle::arith_expr new_uncertain_real(utils::rational &&lb, utils::rational &&ub) override;
-
-    [[nodiscard]] riddle::arith_expr new_time() override;
-    [[nodiscard]] riddle::arith_expr new_time(utils::rational &&value) override;
-
-    [[nodiscard]] utils::inf_rational arith_value(const riddle::arith_term &expr) const noexcept override;
-
-    [[nodiscard]] riddle::string_expr new_string() override;
-    [[nodiscard]] riddle::string_expr new_string(std::string &&value) override;
-    [[nodiscard]] std::string string_value(const riddle::string_term &expr) const noexcept override;
-
-    [[nodiscard]] std::vector<riddle::expr> enum_value(const riddle::enum_term &expr) const noexcept override;
-
-    [[nodiscard]] riddle::arith_expr new_negation(riddle::arith_expr xpr) override;
-
-    [[nodiscard]] riddle::arith_expr new_sum(std::vector<riddle::arith_expr> &&xprs) override;
-    [[nodiscard]] riddle::arith_expr new_subtraction(std::vector<riddle::arith_expr> &&xprs) override;
-    [[nodiscard]] riddle::arith_expr new_product(std::vector<riddle::arith_expr> &&xprs) override;
-    [[nodiscard]] riddle::arith_expr new_division(std::vector<riddle::arith_expr> &&xprs) override;
-
-    virtual void solve() = 0;
-
-    [[nodiscard]] bool match(riddle::term &lhs, riddle::term &rhs) const;
-
-  protected:
-    [[nodiscard]] bool execute(const riddle::bool_expr &expr, std::optional<std::reference_wrapper<context>> ctx = std::nullopt) noexcept;
-
-  private:
-    [[nodiscard]] riddle::atom_state get_atom_state(const riddle::atom_term &atom) const noexcept override;
-
-  protected:
-    arc_consistency::solver ac_slv; // The arc consistency solver..
-    linspire::solver lin_slv;       // The linear programming solver..
-  private:
   };
 } // namespace ratio
--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,64 @@
-#ifdef ORATIO_BUILD_SERVER
-#ifdef SOLVER_BASIC
-#include "basic_server.hpp"
-#endif
-#include <thread>
-#else
-#ifdef SOLVER_BASIC
-#include "basic_solver.hpp"
-#endif
-#include <fstream>
-#endif
-#include "logging.hpp"
+// #ifdef ORATIO_BUILD_SERVER
+// #ifdef SOLVER_BASIC
+// #include "basic_server.hpp"
+// #endif
+// #include <thread>
+// #else
+// #ifdef SOLVER_BASIC
+// #include "basic_solver.hpp"
+// #endif
+// #include <fstream>
+// #endif
+// #include "logging.hpp"
+#include "solver_core.hpp"
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 3)
-    {
-        LOG_FATAL("usage: oRatio <input-file> [<input-file> ...] <output-file>");
-        return -1;
-    }
+//     if (argc < 3)
+//     {
+//         LOG_FATAL("usage: oRatio <input-file> [<input-file> ...] <output-file>");
+//         return -1;
+//     }
 
-    // the problem files..
-    std::vector<std::string> prob_names;
-    for (int i = 1; i < argc - 1; i++)
-    {
-        LOG_DEBUG("adding problem file: " + std::string(argv[i]));
-        prob_names.push_back(argv[i]);
-    }
+//     // the problem files..
+//     std::vector<std::string> prob_names;
+//     for (int i = 1; i < argc - 1; i++)
+//     {
+//         LOG_DEBUG("adding problem file: " + std::string(argv[i]));
+//         prob_names.push_back(argv[i]);
+//     }
 
-    // the solution file..
-    std::string sol_name = argv[argc - 1];
-    LOG_DEBUG("setting solution file: " + sol_name);
+//     // the solution file..
+//     std::string sol_name = argv[argc - 1];
+//     LOG_DEBUG("setting solution file: " + sol_name);
 
-#ifdef ORATIO_BUILD_SERVER
-    ratio::server solver;
-    LOG_INFO("starting oRatio server");
-    auto srv_ft = std::async(std::launch::async, [&solver]
-                             { solver.start(); });
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-#else
-    ratio::solver solver;
-#endif
+// #ifdef ORATIO_BUILD_SERVER
+//     ratio::server solver;
+//     LOG_INFO("starting oRatio server");
+//     auto srv_ft = std::async(std::launch::async, [&solver]
+//                              { solver.start(); });
+//     std::this_thread::sleep_for(std::chrono::seconds(1));
+// #else
+//     ratio::solver solver;
+// #endif
 
-    solver.read(prob_names);
+//     solver.read(prob_names);
 
-    try
-    {
-        solver.solve();
-        LOG_INFO("hurray!! we have found a solution..");
+//     try
+//     {
+//         solver.solve();
+//         LOG_INFO("hurray!! we have found a solution..");
 
-        std::ofstream sol_file;
-        sol_file.open(sol_name);
-        sol_file << solver.to_json().dump();
-        sol_file.close();
-    }
-    catch (const std::exception &ex)
-    {
-        LOG_FATAL("the problem is unsolvable: " + std::string(ex.what()));
-        return -1;
-    }
+//         std::ofstream sol_file;
+//         sol_file.open(sol_name);
+//         sol_file << solver.to_json().dump();
+//         sol_file.close();
+//     }
+//     catch (const std::exception &ex)
+//     {
+//         LOG_FATAL("the problem is unsolvable: " + std::string(ex.what()));
+//         return -1;
+//     }
 
     return 0;
 }
